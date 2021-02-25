39db994570ed6cf5e7d129999ba066b7acc43bdb
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-25 21:47:12 +0900

    リファクタリング

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    validates :title, presence: true, length: { maximum: 10 }
+  belongs_to :user
+  validates :title, presence: true, length: { maximum: 10 }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,35 +4,35 @@ RSpec.describe Post, type: :model do
   before do
     @post = FactoryBot.build(:post)
   end
-  
-  describe "メモ投稿" do
-    context "メモ投稿がうまくいく場合" do
-      it "titleとbodyがあれば投稿できる" do
+
+  describe 'メモ投稿' do
+    context 'メモ投稿がうまくいく場合' do
+      it 'titleとbodyがあれば投稿できる' do
         expect(@post).to be_valid
       end
 
-      it "bodyなしても投稿できる" do
+      it 'bodyなしても投稿できる' do
         @post.body = nil
         expect(@post).to be_valid
       end
 
-      it "titleが10文字以内なら投稿できる" do
-        @post.title = "aaaaaaaaaa"
+      it 'titleが10文字以内なら投稿できる' do
+        @post.title = 'aaaaaaaaaa'
         expect(@post).to be_valid
       end
     end
 
-    context "メモ投稿がうまくいかない場合" do
-      it "titleが空では投稿できない" do
+    context 'メモ投稿がうまくいかない場合' do
+      it 'titleが空では投稿できない' do
         @post.title = nil
         @post.valid?
         expect(@post.errors.full_messages).to include "Title can't be blank"
       end
 
-      it "titleが10以上であれば投稿できない" do
-        @post.title = "aaaaaaaaaaa"
+      it 'titleが10以上であれば投稿できない' do
+        @post.title = 'aaaaaaaaaaa'
         @post.valid?
-        expect(@post.errors.full_messages).to include "Title is too long (maximum is 10 characters)"
+        expect(@post.errors.full_messages).to include 'Title is too long (maximum is 10 characters)'
       end
     end
   end
