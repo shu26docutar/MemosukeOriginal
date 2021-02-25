02951c35685bb0a3cc12fb9ff45b074d7768f165
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-25 11:51:44 +0900

    メモ機能バリデーションの追加・DBの変更

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,3 +1,5 @@
 class Memo < ApplicationRecord
     belongs_to :user
+
+    validates :title, presence: true, length: {maximum:10}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_02_24_082140) do
 
   create_table "memos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
