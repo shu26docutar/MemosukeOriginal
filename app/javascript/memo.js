be4f6f3242a0f7b9ce619bf28b1c65e15ea40af5
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-26 01:10:33 +0900

    メモ投稿フォームのオン・オフ設定

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,8 +1,13 @@
 function memo () {
     const iconBtn = document.getElementById('add-btn');
     const memoArea = document.getElementById('memo-area');
+    const cancel = document.getElementById('cancel-btn');
     iconBtn.addEventListener('click' , () => {
         memoArea.setAttribute("style", "display:block;");
+    });
+
+    cancel.addEventListener('click', () => {
+        memoArea.setAttribute("style", "display:none;");
     });
 };
 
