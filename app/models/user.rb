ac32005abaecaa12fabab58a9984aebf178bb0d0
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-26 13:22:39 +0900

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :sns_credentials
+  has_many :events
 
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
