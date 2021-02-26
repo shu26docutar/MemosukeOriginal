ce503fa6892c84c963ed7f0544af1db384146146
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-26 13:22:54 +0900

    DBマイグレーション

---
+++ db/migrate/20210226040120_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :events do |t|
+      t.string :title,       null: false
+      t.string :plan
+      t.string :comment
+      t.string :url
+      t.string :place
+      t.datetime :start_time
+      t.references :user,    foreign_key: true
+      t.timestamps
+    end
+  end
+end
