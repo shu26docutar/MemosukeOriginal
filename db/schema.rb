91de5719e7acd1cac2013315e2e43d0bf0b45618
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-26 13:22:54 +0900

    DBマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_092552) do
+ActiveRecord::Schema.define(version: 2021_02_26_040120) do
+
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "plan"
+    t.string "comment"
+    t.string "url"
+    t.string "place"
+    t.datetime "start_time"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -44,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_092552) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "sns_credentials", "users"
 end
