14360cf723dfe73183f5f46fdfa07cdec7f1e7df
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-26 16:52:20 +0900

    メモフロント実装

---
+++ app/javascript/event.js
@@ -0,0 +1,20 @@
+//comment
+$(function () {
+    $('.btn-comment').on('click', () => {
+        $('#comment').show();
+    });
+});
+
+//place
+$(function () {
+    $('.btn-place').on('click', () => {
+        $('#place').show();
+    });
+});
+
+//url
+$(function () {
+    $('.btn-url').on('click', () => {
+        $('#url').show();
+    });
+});
