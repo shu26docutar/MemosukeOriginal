0361388098c4ad620aee7b8ebc6739037f9b35c3
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-02-27 19:42:49 +0900

    eventモデルテスト実装

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true
+  with_options presence: true do
+    validates :title
+    validates :start_time
+  end
 end
--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :event do
+    title       { Faker::Lorem.word }
+    plan        { Faker::Lorem.sentence }
+    comment     { Faker::Lorem.sentence}
+    url         { Faker::Internet.url }
+    place       { Faker::Address.city }
+    start_time  { Faker::Date.between_except(from: 1.year.ago, to: 1.year.from_now, excepted: Date.today) }
+    id          { 1 }
+    user_id     { 1 }
+    association :user
   end
 end
