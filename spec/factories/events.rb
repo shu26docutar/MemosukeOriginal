092d0d5ee70066ca588b6ac9ce3efc5834427b77
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-03-02 08:54:03 +0900

    スケジュール投稿結合テストコード実装

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :event do
     title       { Faker::Lorem.word }
-    plan        { Faker::Lorem.sentence }
-    comment     { Faker::Lorem.sentence}
+    plan        { Faker::Lorem.word }
+    comment     { Faker::Lorem.word }
     url         { Faker::Internet.url }
     place       { Faker::Address.city }
     start_time  { Faker::Date.between_except(from: 1.year.ago, to: 1.year.from_now, excepted: Date.today) }
--- spec/system/events_spec.rb
+++ spec/system/events_spec.rb
@@ -2,20 +2,61 @@ require 'rails_helper'
 
 RSpec.describe 'スケジュール', type: :system do
   before do
+    @user = FactoryBot.create(:user)
     @event = FactoryBot.build(:event)
   end
 
   context 'スケジュールが登録できるとき' do
     it 'ログインしているユーザーがスケジュールを作成できるとき' do
-      sign_in(@event.user)
+      sign_in(@user)
+      
+      expect(page).to have_content('translation missing: ja.devise.sessions.user.signed_in') 
       expect(page).to have_content('add')
-      find('events-btn').click
-      expect(current_path).to (new_event_path)
+
+      find('.events-btn').click
+
+      expect(current_path).to eq (new_event_path)
+
       expect(page).to have_content('メモ')
       expect(page).to have_content('場所')
       expect(page).to have_content('URL')
       expect(page).to have_content('戻る')
       expect(page).to have_button('保存')
+
+      fill_in 'event[title]',	with: @event.title
+      fill_in 'event[plan]', with: @event.plan
+
+      find('.btn-comment').click
+      fill_in 'event[comment]', with: @event.comment
+
+      find('.btn-place').click
+      fill_in 'event[place]', with: @event.place
+
+      find('.btn-url').click
+      fill_in 'event[url]', with: @event.url
+
+      select '2021', from:'event[start_time(1i)]'
+      select '3', from:'event[start_time(2i)]'
+      select '1', from:'event[start_time(3i)]'
+      select '10', from:'event[start_time(4i)]'
+      select '10', from:'event[start_time(5i)]'
+
+      expect{
+        find('#submit-btn').click
+      }.to change { Event.count }.by(1)
+
+      expect(current_path).to eq (root_path)
+
+      expect(page).to have_content(@event_title)
+    end
+  end
+
+  context 'スケジュール登録できないとき' do
+    it 'ログインしていないユーザーは登録ボタンがない' do
+      visit root_path
+      expect(page).to have_no_content('#add-btn')
+      visit new_event_path
+      expect(current_path).to eq (new_user_session_path) 
     end
   end
 end
