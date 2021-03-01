95dab33fa87a6f334ff3beda04a82e27d847eda2
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2021-03-02 00:44:28 +0900

    facker　パスワード半角英数字に変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :user do
     name                  { Faker::Name.initials(number: 4) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     birthday              { Faker::Date.between(from: 2.days.ago, to: Date.today) }
-    # id                    { 1 }
+    id                    { 1 }
   end
 end
